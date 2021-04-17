acd5d39f7a856c08532e19c45a09fe86c6bf4b0a
Saulo Cruz <saulo.cruz@smanuvemlive.onmicrosoft.com>
2021-04-16 23:52:57 -0300

    calculator

---
+++ 02_RestWithASPNET_Calculator/RestWithASPNET/RestWithASPNET/Controllers/CalculatorController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNET.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Get(string firstNumber, string secondNumber)
+        {
+            if(IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = Convert.ToDecimal(firstNumber) + Convert.ToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("Invalid Input");
+        }
+
+        private bool IsNumeric(string strNumber)
+        {
+            return decimal.TryParse(strNumber,
+                System.Globalization.NumberStyles.Any,
+                System.Globalization.NumberFormatInfo.InvariantInfo,
+                out decimal number);
+        }
+    }
+}
